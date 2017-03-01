39d82ad51a96849d1d5f701350cc0f19acf4bbfa
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-01 15:49:52 -0800

    Add keys

--- server.R
+++ server.R
@@ -2,6 +2,9 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
+key <- "52a837c5cf9b4a2d996a0a7468c7a221"
+secret_key <- "2b217e649b1547c6aba4596fdc715784"
+
 server <- function(input, output) {
   
 
