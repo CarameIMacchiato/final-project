3d2d9eb0b1c4909b0536e5b7cffb7be3f46c4166
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-01 15:58:03 -0800

    Add instructions

--- server.R
+++ server.R
@@ -2,8 +2,13 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-key <- "52a837c5cf9b4a2d996a0a7468c7a221"
-secret_key <- "2b217e649b1547c6aba4596fdc715784"
+key <- "?access_token=52a837c5cf9b4a2d996a0a7468c7a221"
+secret.key <- "2b217e649b1547c6aba4596fdc715784"
+
+base.url <- "https://api.instagram.com/v1/"
+
+# example url is https://api.instagram.com/v1/users/self/?access_token=ACCESS-TOKEN
+# Format is base url / request / access key
 
 server <- function(input, output) {
   
