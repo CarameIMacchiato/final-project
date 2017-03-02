cfd89b94840d78000ce439ee169459093b8511b5
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-01 16:47:45 -0800

    Update readme with features

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 This is the ReadMe!
+
+Features to make!
+
+- Map
+  - create map with google maps api, if possible
+- User search
+  - search by user name. create a mini profile viewer
+- Bar graph of filters
+  - bar graph of most popular filters
+- Heatmap
+  - heatmap of likes, tags, etc.
+- Follow Feature
+  - have people get a follower on their account
+- Comment Feature
+  - put a comment on an image
--- server.R
+++ server.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
-access.token <- "access_token=1942144530.52a837c.52b8ff2e81a5417688ef1fd8c1c5d339"
+access.token <- "?access_token=1942144530.52a837c.52b8ff2e81a5417688ef1fd8c1c5d339"
 
 base.url <- "https://api.instagram.com/v1/"
 
@@ -12,10 +12,10 @@ base.url <- "https://api.instagram.com/v1/"
 # Format is base url / request / access key
 
 
-response <- GET("https://api.instagram.com/v1/users/self/?access_token=52a837c5cf9b4a2d996a0a7468c7a221")
+response <- GET(paste0(base.url, "users/self/", access.token))
 body <- fromJSON(content(response, "text"))
 
-body$meta
+a <- body$data
 
 server <- function(input, output) {
   
