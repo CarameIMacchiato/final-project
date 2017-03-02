ee915c9de8787bac90ff8f52523b6e74416ba430
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-01 16:17:52 -0800

    Get real token

--- server.R
+++ server.R
@@ -4,14 +4,18 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
-key <- "?access_token=52a837c5cf9b4a2d996a0a7468c7a221"
-secret.key <- "2b217e649b1547c6aba4596fdc715784"
+access.token <- "access_token=1942144530.52a837c.52b8ff2e81a5417688ef1fd8c1c5d339"
 
 base.url <- "https://api.instagram.com/v1/"
 
 # example url is https://api.instagram.com/v1/users/self/?access_token=ACCESS-TOKEN
 # Format is base url / request / access key
 
+
+response <- GET("https://api.instagram.com/v1/users/self/?access_token=52a837c5cf9b4a2d996a0a7468c7a221")
+body <- fromJSON(content(response, "text"))
+
+body$meta
 
 server <- function(input, output) {
   
