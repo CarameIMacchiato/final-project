2ee509d5efec7755bc939061752390d6cb8b8ba3
Mario Paolini <trepaolini@gmail.com>
2017-03-01 17:00:36 -0800

    adds barebone UI

--- ui.R
+++ ui.R
@@ -6,4 +6,21 @@ library(dplyr)
 # Defining the UI
 ui <- fluidPage(
   
+  titlePanel("Instagram Project"),
+  
+  sidebarPanel(
+    
+  ),
+  
+  mainPanel(
+    navbarPage(
+               tabPanel("Map"),
+               tabPanel("Search"),
+               tabPanel("Heat Map"),
+               tabPanel("Statistics"),
+               tabPanel("Mini Profile Page")
+               
+    )
+  )
 )
+  
