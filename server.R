121878683812655a6846739c5f74486a2a3c0938
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-01 16:02:26 -0800

    Add libraries

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(httr)
+library(jsonlite)
 
 key <- "?access_token=52a837c5cf9b4a2d996a0a7468c7a221"
 secret.key <- "2b217e649b1547c6aba4596fdc715784"
@@ -9,6 +11,7 @@ base.url <- "https://api.instagram.com/v1/"
 
 # example url is https://api.instagram.com/v1/users/self/?access_token=ACCESS-TOKEN
 # Format is base url / request / access key
+
 
 server <- function(input, output) {
   
