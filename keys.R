46ec3e83fc27a568cae991eca2b0335c7db8255f
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-03-07 16:37:51 -0800

    Clean UI

--- keys.R
+++ keys.R
@@ -1,5 +1,5 @@
-access.token <- "access_token=1942144530.52a837c.52b8ff2e81a5417688ef1fd8c1c5d339"
+#access.token <- "access_token=1942144530.52a837c.52b8ff2e81a5417688ef1fd8c1c5d339"
 # second access token
-# access.token <- "access_token=4764046825.52a837c.79251ae83c064d1fb685e52a75743f9c"
+ access.token <- "access_token=4764046825.52a837c.79251ae83c064d1fb685e52a75743f9c"
 client.id <- "52a837c5cf9b4a2d996a0a7468c7a221"
 secret.key <- "2b217e649b1547c6aba4596fdc715784"
