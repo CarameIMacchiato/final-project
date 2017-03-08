baecbec74cf3b166db667a8265ae490820c8e121
Mario Paolini <trepaolini@gmail.com>
2017-03-08 10:30:47 -0800

    add abbys token

--- keys.R
+++ keys.R
@@ -2,6 +2,9 @@
 # second access token
 # access.token <- "access_token=4764046825.52a837c.79251ae83c064d1fb685e52a75743f9c"
 # third access token
- access.token <- "access_token=4769425886.52a837c.5563101c59594d61bb90c5919c2b3a16"
+access.token <- "access_token=4769425886.52a837c.5563101c59594d61bb90c5919c2b3a16"
 client.id <- "52a837c5cf9b4a2d996a0a7468c7a221"
 secret.key <- "2b217e649b1547c6aba4596fdc715784"
+
+# abby's possibly broken token
+# access.token <- "access_token=200807758.52a837c.40873fe55370442e8c64a490f0832a19"
